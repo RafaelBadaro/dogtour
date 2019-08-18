f6c5176b71dd601b68a22b49ada3c9c6b839ee12
Rafael Badaro <rafael.badaro@mrv.com.br>
2019-08-18 20:19:08 -0300

    Commit inicial  - 3 tabs que o sistema terá

---
+++ Frontend/dogtourApp/src/app/tabs/tabs.router.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'passeioTab',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../passeioTab/passeio.module').then(m => m.PasseioPageModule)
+          }
+        ]
+      },
+      {
+        path: 'historicoTab',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../historicoTab/historico.module').then(m => m.HistoricoPageModule)
+          }
+        ]
+      },
+      {
+        path: 'contaTab',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../contaTab/conta.module').then(m => m.ContaPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/passeioTab',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/passeioTab',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
