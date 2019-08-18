5212934c9814570c8e8d6601ae877ce9b8aeeb6d
Rafael Badaro <rafael.badaro@mrv.com.br>
2019-08-18 20:19:08 -0300

    Commit inicial  - 3 tabs que o sistema terá

---
+++ src/app/contaTab/conta.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conta',
+  templateUrl: 'conta.page.html',
+  styleUrls: ['conta.page.scss']
+})
+export class ContaPage {
+
+  constructor() {}
+
+}
---
+++ src/app/passeioTab/passeio.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passeio',
+  templateUrl: 'passeio.page.html',
+  styleUrls: ['passeio.page.scss']
+})
+export class PasseioPage {
+
+  constructor() {}
+
+}
