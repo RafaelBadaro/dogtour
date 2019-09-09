30b2123ed621ec34f1daaad3afea2d4546f1ce2b
Rafael Badaro <rafael.badaro@mrv.com.br>
2019-09-09 17:50:33 -0300

    Roteamento com bloqueio de rotas

--- Frontend/dogtourApp/src/app/app-routing.module.ts
+++ Frontend/dogtourApp/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
+
+  {
+    path: '' || 'login',
+    component: LoginComponent,
+  },
+
   {
     path: '',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
