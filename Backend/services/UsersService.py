d2b7073b993785d7f5e8260d412812225d4a9f76
Lucas Rotsen Pereira <lucasrotsen@gmail.com>
2019-09-15 19:09:31 -0300

    Finalizada implementação do cadastro de usuários

---
+++ Backend/services/UsersService.py
@@ -0,0 +1,61 @@
+import pyrebase
+from nameko.rpc import rpc
+from utils.db.db import config
+from nameko.web.handlers import http
+
+firebase = pyrebase.initialize_app(config)
+
+auth = firebase.auth()
+db = firebase.database()
+
+class UsersService:
+    name = "users"
+
+    @rpc
+    def create(self, name, email, password, role):
+
+        fEmail = email.replace(".", ",")
+
+        if db.child("users").child(fEmail).get().val() is None:
+
+            user = auth.create_user_with_email_and_password(email, password)
+            idToken = user['idToken']
+
+            data = {
+                "name": name,
+                "role": role
+            }
+
+            db.child("users").child(fEmail).set(data, idToken)
+
+            errorMsg = ""
+            return (idToken, errorMsg)
+        
+        errorMsg = "Usuario ja cadastrado!"
+        idToken = ""
+
+        return (idToken, errorMsg)
+
+    @rpc
+    def login(self, email, password):
+
+        fEmail = email.replace(".", ",")
+
+        if db.child("users").child(fEmail).get().val() is not None:
+
+            user = auth.sign_in_with_email_and_password(email, password)
+            idToken = user['idToken']
+
+            name = db.child("users").child(fEmail).child("name").get().val()
+
+            errorMsg = ""
+            return (name, idToken, errorMsg)
+        
+        
+        name = ""
+        idToken = ""
+        errorMsg = "Usuario nao cadastrado!"
+
+        return (name, idToken, errorMsg)
+
+    
