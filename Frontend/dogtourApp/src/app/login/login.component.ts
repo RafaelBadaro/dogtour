8d9e0096de85bf2fb2445104c97171bc4efcd159
Lucas Rotsen Pereira <lucasrotsen@gmail.com>
2019-09-25 18:42:45 -0300

    Autenticacao por token do frontend

--- Frontend/dogtourApp/src/app/login/login.component.ts
+++ Frontend/dogtourApp/src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -17,7 +18,8 @@ export class LoginComponent implements OnInit {
 
   password = new FormControl('',Validators.required);
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private http: HttpClient) { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private http: HttpClient, 
+    private authService: AuthService) { }
 
   ngOnInit() {
 
@@ -30,16 +32,20 @@ export class LoginComponent implements OnInit {
   realizarLogin() {
 
     this.http.post('/api/user/login', this.formLogin.value).subscribe(
-      (res) => {
-        console.log(res);
+      (res : any) => {
+        this.authService.usuarioAuth.email = this.email.value;
+        this.authService.usuarioAuth.name = res.name;
+        
+        this.email.setValue(undefined)
+        this.password.setValue(undefined);
+
+        sessionStorage.setItem('token', res.idToken);
         this.router.navigate(['/tabs/passeioTab']);
       },
       (err) => {
         console.log(err);
       },
-      ()=>{
-        console.log('finalizei');
-      }
+      ()=>{}
     );
   }
 
