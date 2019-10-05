30be01404693a4080a4a0d153aa4d04a0eba2cab
Rafael Badaró <rafabadaro.15@gmail.com>
2019-10-05 19:59:22 -0300

    layout tela de conta do dono

--- Frontend/dogtourApp/src/app/contaTab/conta.page.ts
+++ Frontend/dogtourApp/src/app/contaTab/conta.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Usuario } from '../models/usuario.model';
 
 @Component({
   selector: 'app-conta',
   templateUrl: 'conta.page.html',
   styleUrls: ['conta.page.scss']
 })
-export class ContaPage {
+export class ContaPage implements OnInit  {
 
-  constructor() {}
+  usuario: Usuario;
 
+  cachorrosUsuario: any[];
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.usuario =  this.authService.usuarioAuth;
+  }
+  
 }
