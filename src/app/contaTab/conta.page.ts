b60bb659dd9429ecfccec8bc5b3bdf0acffdd849
Rafael Badaro <rafael.badaro@mrv.com.br>
2019-10-07 19:38:14 -0300

    Ajustes na tela da conta do dono

--- src/app/contaTab/conta.page.ts
+++ src/app/contaTab/conta.page.ts
@@ -1,22 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Usuario } from '../models/usuario.model';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-conta',
   templateUrl: 'conta.page.html',
   styleUrls: ['conta.page.scss']
 })
-export class ContaPage implements OnInit  {
+export class ContaPage implements OnInit {
+
+  formConta: FormGroup;
+  name = new FormControl('');
+  email = new FormControl('');
+  password = new FormControl('');
 
   usuario: Usuario;
-
   cachorrosUsuario: any[];
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.usuario =  this.authService.usuarioAuth;
+    this.usuario = this.authService.usuarioAuth;
+
+    this.formConta = this.formBuilder.group({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+    });
   }
-  
+
+  atualizarDados() {
+    console.log(this.formConta.value);
+  }
+
 }
