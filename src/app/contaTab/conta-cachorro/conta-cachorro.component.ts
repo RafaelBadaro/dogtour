eb7f7843b5a712479888dda979e393c1e516b0d9
Rafael Badaro <rafael.badaro@mrv.com.br>
2019-10-07 21:02:19 -0300

    Criando component filho para lista de cachorros na contaTab

---
+++ src/app/contaTab/conta-cachorro/conta-cachorro.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conta-cachorro',
+  templateUrl: './conta-cachorro.component.html',
+  styleUrls: ['./conta-cachorro.component.scss'],
+})
+export class ContaCachorroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/models/cachorro.model.ts
@@ -0,0 +1,5 @@
+export class Cachorro {
+    name: string;
+
+    race: string;
+}
