833b153caf86e32eb99fb41d8a84893281ae0175
Rafael Badaró <rafabadaro.15@gmail.com>
2019-11-10 19:59:14 -0300

    Limpando código

--- Frontend/dogtourApp/src/app/contaTab/conta-cachorro/conta-cachorro.component.ts
+++ Frontend/dogtourApp/src/app/contaTab/conta-cachorro/conta-cachorro.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Component, OnInit, Input} from '@angular/core';
 import { Cachorro } from 'src/app/models/cachorro.model';
 
 @Component({
@@ -7,17 +6,11 @@ import { Cachorro } from 'src/app/models/cachorro.model';
   templateUrl: './conta-cachorro.component.html',
   styleUrls: ['./conta-cachorro.component.scss'],
 })
-export class ContaCachorroComponent implements OnInit {
+export class ContaCachorroComponent {
 
   @Input() dog: Cachorro;
 
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
-  enviarAlteracoes() {
-  }
 
 }
--- Frontend/dogtourApp/src/app/passeioTab/passeio.page.ts
+++ Frontend/dogtourApp/src/app/passeioTab/passeio.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoadingService } from '../services/loading.service';
 import { AlertService } from '../services/alert.service';
 import { constantes } from '../constantes';
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
   templateUrl: 'passeio.page.html',
   styleUrls: ['passeio.page.scss']
 })
-export class PasseioPage {
+export class PasseioPage  implements OnInit {
 
   formAgendamento: FormGroup;
 
@@ -29,24 +29,12 @@ export class PasseioPage {
       passeador: this.passeador,
       horario: this.horario
     });
-    const u = new Usuario();
-    u.name = 'Maca';
-    u.role = 'a';
-    u.email = 'email';
-    u.dogs = [];
-    u.horarios = ['Data e hora 1', 'Data e hora 2'];
-
-    this.passeadores.push(u);
-
-    const u2 = new Usuario();
-    u2.name = 'Willer';
-    u2.role = 'a';
-    u2.email = 'email';
-    u2.dogs = [];
-    u2.horarios = ['Data e hora 3', 'Data e hora 4'];
-
-    this.passeadores.push(u2);
   }
+
+  ngOnInit(): void {
+    
+  }
+ 
 
   public agendar() {
     this.passeioAgendado = true;
